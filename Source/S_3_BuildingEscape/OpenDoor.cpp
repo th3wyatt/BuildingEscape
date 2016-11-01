9345afd61c2816a1e14ce1d2cf9e38e929521a47
Jonathan Wyatt <jonathan.k.wyatt@gmail.com>
2016-10-31 21:36:14 -0700

    Added code for opening the door when stepping on a pressure plate.

--- Source/S_3_BuildingEscape/OpenDoor.cpp
+++ Source/S_3_BuildingEscape/OpenDoor.cpp
@@ -21,20 +21,27 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	AActor* Owner = GetOwner();
-
-	FRotator NewRotation = FRotator(0.0f, 60.0f, 0.0f);
-
-	Owner->SetActorRotation(NewRotation);
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 	
 }
 
+void UOpenDoor::OpenDoor() {
+
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+
+}
 
 // Called every frame
 void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+	}
 }
 
